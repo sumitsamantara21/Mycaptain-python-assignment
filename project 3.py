d5bc833a7d7191397fb7c976638a371bba6e5a54
sumit samantara <121337160+sumitsamantara21@users.noreply.github.com>
2023-10-21 02:43:36 +0530

    Delete project 3.py

--- project 3.py
+++
@@ -1,9 +0,0 @@
-start = int(input("Enter the starting number: "))
-end = int(input("Enter the ending number: "))
-if start > end:
-    print("Invalid range. Starting number should be less than or equal to the ending number.")
-else:
-    print("Positive numbers in the range from", start, "to", end, "are:")
-    for number in range(start, end + 1):
-        if number > 0:
-            print(number)
